a8770f6b7b499e56e80bd538fc368d80c402e296
Steve Taylor <steve.taylor@openmakesoftware.com>
2017-09-21 10:45:46 -0600

    Initial Load

---
+++ src/main/java/InitServer.java
@@ -0,0 +1,59 @@
+ package uptime;
+// TEST 
+ import java.io.IOException;
+ import java.io.PrintWriter;
+ import java.util.Date;
+ import javax.servlet.ServletConfig;
+ import javax.servlet.ServletContext;
+ import javax.servlet.ServletException;
+ import javax.servlet.http.HttpServlet;
+ import javax.servlet.http.HttpServletRequest;
+ import javax.servlet.http.HttpServletResponse;
+ import javax.servlet.jsp.JspEngineInfo;
+ import javax.servlet.jsp.JspFactory;
+ 
+ public class InitServer extends HttpServlet
+ {
+   private static final long serialVersionUID = -3720115042877301162L;
+   private String message;
+ 
+   public void init()
+     throws ServletException
+   {
+    System.setProperty("start", new Long(new Date().getTime()).toString());
+   }
+ 
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+     throws ServletException, IOException
+   {
+     response.setContentType("text/html");
+ 
+     PrintWriter out = response.getWriter();
+     String x = System.getProperty("start");
+     String Node = "Application";
+     long milliSeconds = Long.parseLong(x);
+     Date start = new Date(milliSeconds);
+     Date now = new Date();
+ 
+     long diff = Math.abs(now.getTime() - start.getTime()) / 1000L;
+     ServletContext application = getServletConfig().getServletContext();
+     this.message = 
+       ("<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"default.css\"><title>Uptime</title></head><body><div id=\"outer\"> <div id=\"header\">  <h1><a href=\"#\">Tomcat Node Status</a></h1>" + 
+       " </div>" + 
+       " <div class=\"main\"><table><tr><td align=\"right\">Tomcat Version :</td><td>" + application.getServerInfo() + "</td></tr>");
+     this.message = (this.message + "<tr><td align=\"right\">Servlet Specification Version :</td><td>" + application.getMajorVersion() + "." + application.getMinorVersion() + "</td></tr>");
+     this.message = (this.message + "<tr><td align=\"right\">JSP version :</td><td>" + JspFactory.getDefaultFactory().getEngineInfo().getSpecificationVersion() + "</td></tr>");	 	 
+     this.message = (this.message + "<tr><td align=\"right\">Start Time :</td><td>" + start.toString() + "</td></tr>");
+     this.message += "<tr><td align=\"right\">&nbsp;</td><td>&nbsp;</td></tr>";
+     this.message += "<tr><td align=\"right\">Application Version :</td><td>3.3</td></tr>";
+     this.message = (this.message + "<tr><td align=\"right\">Up Time :</td><td>" + diff + " seconds</td></tr>");
+ 
+     this.message += "</table>";
+     out.println(this.message + "</div></div></body></html>");
+   }
+ 
+   public void destroy()
+   {
+   }
+ }
+
